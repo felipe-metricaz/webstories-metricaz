ec19baa79acbc69561875cb66dc08cbf4957365c
felipe152 <felipe.peixoto@metricaz.com>
2023-01-26 11:24:05 -0300

    uso de rotas

---
+++ src/pages/Back.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Back() {
+    return (
+        <div>
+            <h1>This is the back page</h1>
+        </div>
+    )
+}
+
+export default Back
---
+++ src/pages/Front.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Front() {
+    return (
+        <div>
+            <h1>This is the Front page</h1>
+        </div>
+    )
+}
+
+export default Front
